72bce05363cc0cf8077563bfd1bb71858f58c19c
TLynam <TLynam@gmail.com>
2010-12-29 02:28:08 -0800

    Validated Date and fixed left margin for Program Show

--- app/models/date_of_workout.rb
+++ app/models/date_of_workout.rb
@@ -1,4 +1,6 @@
 class DateOfWorkout < ActiveRecord::Base
   has_many :exercises, :dependent => :destroy
   accepts_nested_attributes_for :exercises, :allow_destroy => true
+
+  validates_format_of :date, :with => /\d{4}\-\d{2}\-\d{2}/, :message => "Date must be in the format yyyy/mm/dd"
 end
